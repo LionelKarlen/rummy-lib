7cab7356d86022924353454a6b8590fbbc87aa6d
LionelKarlen <lionel.karlen@protonmail.com>
2021-11-13 23:23:11 +0100

    Export items for lib

--- index.ts
+++ index.ts
@@ -1,2 +1,38 @@
-function main() {
-}
+import { Board } from "./src/Board";
+import { Card } from "./src/Card";
+import { Meld, Run, Book } from "./src/Meld";
+import { MeldType } from "./src/MeldType";
+import { Move, PickupMove, PutdownMove, MeldMove, LayMove } from "./src/Move";
+import { Player } from "./src/Player";
+import { Rank } from "./src/Rank";
+import { Rummy } from "./src/Rummy";
+import { Score } from "./src/Score";
+import { Stack, Hand, Stock, Discard, MeldStack } from "./src/Stack";
+import { StackType } from "./src/StackType";
+import { Suit } from "./src/Suit";
+import { Util } from "./src/Util";
+export {
+	Board,
+	Card,
+	Meld,
+	MeldType,
+	Move,
+	Player,
+	Rank,
+	Rummy,
+	Score,
+	Stack,
+	StackType,
+	Suit,
+	Util,
+	Run,
+	Book,
+	PickupMove,
+	PutdownMove,
+	MeldMove,
+	LayMove,
+	Hand,
+	Stock,
+	Discard,
+	MeldStack,
+};
