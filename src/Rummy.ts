b8b7b8023d99dff40016589aa1653ab0fd7ed0b3
LionelKarlen <lionel.karlen@protonmail.com>
2021-11-17 21:49:25 +0100

    Add stock refill debug prints

--- src/Rummy.ts
+++ src/Rummy.ts
@@ -24,11 +24,13 @@ export class Rummy {
 	makeMove(player: Player, move: Move) {
 		move.makeMove();
 		if (this.board.stock.cards.length == 1) {
+			console.log("stock empty",this.board.stock.cards.length);
 			this.board.stock.cards = this.board.discard.cards.splice(
 				0,
 				this.board.discard.cards.length - 2
 			);
 			this.board.stock.cards = Util.shuffleCards(this.board.stock.cards);
+			console.log("stock filled",this.board.stock.cards.length);
 		}
 		if (Util.checkGameOver(player.hand)) {
 			this.handleGameOver();
